21a41ec8076f406240745cd7fa14bd89bc9755af
jpd61 <jpd@dewoodyoil.com>
2020-08-02 17:34:19 -0500

    Updates

---
+++ precache-manifest.4d3c65a493ec983c37b9a243d53abeee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876f2696186fbf921b6f71c7e18de876",
+    "url": "/jpd-react-portfolio/index.html"
+  },
+  {
+    "revision": "f253071595cf489e16fe",
+    "url": "/jpd-react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0720df3785a50d110f95",
+    "url": "/jpd-react-portfolio/static/css/main.b0f15f8c.chunk.css"
+  },
+  {
+    "revision": "f253071595cf489e16fe",
+    "url": "/jpd-react-portfolio/static/js/2.3a139711.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jpd-react-portfolio/static/js/2.3a139711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0720df3785a50d110f95",
+    "url": "/jpd-react-portfolio/static/js/main.fbb7bf8a.chunk.js"
+  },
+  {
+    "revision": "0a7af82412b51fc5009c",
+    "url": "/jpd-react-portfolio/static/js/runtime-main.50eebf1b.js"
+  }
+]);
